fbf758de8c256e96a15a863b27771b133f027806
Olya <87827610+Alipiya@users.noreply.github.com>
2022-01-17 23:42:27 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Message, message } from './components';
+
+function App() {
+  return (
+    <div className='App'>
+      <Message
+        messageProps={'А вот и наоборот :)'}>
+      </Message>
+    </div>
+  )
+}
+
+export default App;
