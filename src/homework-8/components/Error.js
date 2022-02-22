3a37e7e1477093c7c66e04ebd9d1ea9afc1d54c5
Olya <87827610+Alipiya@users.noreply.github.com>
2022-02-22 15:57:43 +0300

    Delete src/homework-8 directory

--- src/homework-8/components/Error.js
+++
@@ -1,9 +0,0 @@
-import Alert from '@material-ui/lab/Alert';
-import {Button} from '@material-ui/core';
-
-export const Error = ({ reload }) => (
-  <Alert severity="error">
-    Oops, something went wrong!
-    <Button onClick={reload}>Reload</Button>
-  </Alert>
-);
